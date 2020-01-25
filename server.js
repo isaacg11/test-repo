c70135e28bce2a21be59d7bd8cd184f565d115b7
isaacgrey11 <isaac.j.grey@gmail.com>
2020-01-25 09:41:59 -0800

    fixed typo

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('/api/v1/moves', moves);
 // serve index.html
 if(process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + '/client/build/index.html'))
+        res.sendFile(path.join(__dirname + '/client/index.html'))
     });
 }
 
